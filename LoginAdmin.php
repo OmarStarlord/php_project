be87147d0d0ada3a9686e43b183a25b9c4625d5e
OmarStarlord <126420811+OmarStarlord@users.noreply.github.com>
2023-12-11 19:13:24 +0100

    new

--- LoginAdmin.php
+++ LoginAdmin.php
@@ -1,37 +1,148 @@
 <?php
+
+class Connection {
+    private $servername;
+    private $username;
+    private $password;
+    private $dbname;
+    private $conn;
+
+    public function __construct($servername, $username, $password, $dbname) {
+        $this->servername = $servername;
+        $this->username = $username;
+        $this->password = $password;
+        $this->dbname = $dbname;
+    }
+
+    public function connect() {
+        try {
+            $this->conn = new PDO("mysql:host={$this->servername};dbname={$this->dbname}", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
+
+    public function disconnect() {
+        $this->conn = null;
+    }
+
+    public function executeQuery($query, $params = []) {
+        try {
+            $stmt = $this->conn->prepare($query);
+
+            foreach ($params as $key => $value) {
+                $stmt->bindValue($key, $value);
+            }
+
+            $stmt->execute();
+
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            die("Error in SQL query: " . $e->getMessage());
+        }
+    }
+}
+
+// Usage example:
+
 $servername = "localhost";
 $username = "Omar";
 $password = "omar";
 $dbname = "platformcoursera";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+$connection = new Connection($servername, $username, $password, $dbname);
+$connection->connect();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $password = $_POST["password"];
 
-    $stmt = $conn->prepare("SELECT * FROM admins WHERE AdminID = ? AND Password = ?");
-    
-    if (!$stmt) {
-        die("Error in SQL query: " . $conn->error);
-    }
+    $query = "SELECT * FROM admins WHERE AdminID = :name AND Password = :password";
+    $params = [':name' => $name, ':password' => $password];
 
-    $stmt->bind_param("is", $name, $password);
-    $stmt->execute();
-    $result = $stmt->get_result();
+    $result = $connection->executeQuery($query, $params);
 
-    if ($result->num_rows > 0) {
+    if (count($result) > 0) {
         header("Location: admindashboard/admin_dashboard.php");
         exit();
     } else {
         echo "Invalid admin credentials.";
     }
-    $stmt->close();
 }
 
-$conn->close();
+$connection->disconnect();
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Login</title>
+
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        form {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        h2 {
+            text-align: center;
+            color: #333;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 8px;
+            color: #555;
+        }
+
+        input {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 15px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+
+        input[type="submit"] {
+            background-color: #4caf50;
+            color: #fff;
+            cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #45a049;
+        }
+    </style>
+</head>
+<body>
+    <form method="post">
+        <h2>User Login</h2>
+
+        <label for="name">Admind Id:</label>
+        <input type="text" name="name" required>
+
+        <label for="password">Password:</label>
+        <input type="password" name="password" required>
+
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
